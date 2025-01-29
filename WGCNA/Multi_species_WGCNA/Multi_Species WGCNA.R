a4eda92f09fc63f86004cc40944b1f31e9495793
Amalia Murgueitio Calle <amalia.murgueitio@gmail.com>
2025-01-29 17:41:27 +0200

    Multi-species advance with code

--- WGCNA/Multi_species_WGCNA/Multi_Species WGCNA.R
+++ WGCNA/Multi_species_WGCNA/Multi_Species WGCNA.R
@@ -79,6 +79,8 @@ library("ggplot2")
 library("dplyr")
 
 setwd("C:/Users/amurgueitio/Documents/Multistage_Omics/Multi_species_WGCNA")
+setwd("C:/Users/amurg/OneDrive/Documentos/GitHub/Multistage_omics/WGCNA/Multi_species_WGCNA")
+
 
 # Load necessary library
 library(readr)
@@ -106,4 +108,231 @@ gcount_raw <- read_csv("merged_data_acropora_montipora_pocillopora_spis.csv",
                          SRR14333326 = col_integer(), 
                          SRR14333327 = col_integer()
                        ))
+
+treatmentinfo <- read_csv("treatmentinfo_merged.csv")
+
+#Renaming columns so they're more manageable
+
+# Identify columns that need to be renamed (7 onward)
+old_names <- colnames(gcount_raw)[7:ncol(gcount_raw)]
+
+# Match old names with new_names in treatmentinfo
+new_names <- treatmentinfo$new_names[match(old_names, treatmentinfo$sampleID)]
+
+# Replace only the column names from column 7 onward
+colnames(gcount_raw)[7:ncol(gcount_raw)] <- ifelse(is.na(new_names), old_names, new_names)
+
+#Switching the order for Spis spat
+
+# Define column ranges
+spis_spat_cols <- 34:36  # Current positions of Spis spat
+spis_larvae_cols <- 40:42  # Current positions of Spis larvae
+
+# Swap the columns by reordering
+# Define column ranges
+spis_spat_cols <- 34:36  # Current positions of Spis spat
+spis_larvae_cols <- 40:42  # Current positions of Spis larvae
+
+# Swap the columns
+gcount_raw <- gcount_raw[, c(1:33, spis_larvae_cols, 37:39, spis_spat_cols)]
+
+#Now I'm making a dataframe and calling Orthogroup rownames
+gcount_raw <- as.data.frame(gcount_raw)
+rownames(gcount_raw) <- gcount_raw$Orthogroup  # If gene IDs are in a column
+# Extract only the count data (columns 7 to 42)
+gcount <- gcount_raw[, 7:42]
+
+# Set filter values for PoverA
+filt <- filterfun(pOverA(0.083, 10))
+
+# Apply filter to count data only
+gfilt <- genefilter(gcount, filt)
+
+# Identify genes to keep by count filter
+gkeep <- gcount[gfilt, ]
+
+# Identify gene names to keep
+gn.keep <- rownames(gkeep)
+
+# Subset the original dataset to keep metadata and filtered count data
+gcount_filt <- gcount[rownames(gcount) %in% gn.keep, ]
+
+# Check how many rows before and after filtering
+nrow(gcount)  # Before filtering
+#7129
+nrow(gcount_filt) # After filtering
+#7118
+
+# Normalize our read counts using VST-normalization in DESeq2
+# Construct the DESeq2 dataset
+
+#Merge the timepoint columns into a new column, group. Set group as a factor.
+treatmentinfo$timepoint <- factor(treatmentinfo$timepoint, levels = c("I","II","III"))
+treatmentinfo$Species <- factor(treatmentinfo$Species, levels = c("Acropora_tenuis", "Montipora_capitata", "Pocillopora_acuta","Stylophora_pistillata"))
+
+
+gdds <- DESeqDataSetFromMatrix(countData = gcount_filt,
+                               colData = treatmentinfo,
+                               design = ~ Species * timepoint)
+# Log-transform the count data using a variance stabilizing transforamtion (vst). 
+
+SF.gdds <- estimateSizeFactors( gdds ) #estimate size factors to determine if we can use vst  to transform our data. Size factors should be less than for to use vst
+print(sizeFactors(SF.gdds)) #View size factors
+#print(sizeFactors(SF.gdds)) #View size factors
+#Larva1_Atenuis Larva2_Atenuis Larva3_Atenuis  Meta1_Atenuis  Meta2_Atenuis  Meta3_Atenuis  Spat1_Atenuis  Spat2_Atenuis 
+#2.1484437      2.5045719      2.4650281      1.6930436      1.8298762      1.4530130      2.2826295      2.4530962 
+#Spat3_Atenuis    Larva1_Mcap    Larva2_Mcap    Larva3_Mcap     Meta1_Mcap     Meta2_Mcap     Meta3_Mcap     Spat1_Mcap 
+#1.9067000      2.5355610      3.0515890      3.0873104      2.9781568      2.5851077      3.0724323      0.9420058 
+#Spat2_Mcap     Spat3_Mcap    Larva1_Pacu    Larva2_Pacu    Larva3_Pacu     Meta1_Pacu     Meta2_Pacu     Meta3_Pacu 
+#1.2404501      1.6396740      0.6839530      0.9259933      1.0532392      0.6829982      0.7023775      0.7466266 
+#Spat1_Pacu     Spat2_Pacu     Spat3_Pacu    Larva1_Spis    Larva2_Spis    Larva3_Spis     Meta1_Spis     Meta2_Spis 
+#0.9111311      0.8727706      0.8633586      0.2405015      0.1598825      0.2085578      0.2252045      0.3610082 
+#Meta3_Spis     Spat1_Spis     Spat2_Spis     Spat3_Spis 
+#0.4935508      0.4431366      0.4204062      0.4138748 
+
+gvst <- vst(gdds, blind=FALSE) #apply a variance stabilizing transforamtion to minimize effects of small counts and normalize wrt library size
+
+# Transpose the filtered gene count matrix so that the gene IDs are rows and the sample IDs are columns.
+
+datExpr <- as.data.frame(t(assay(gvst))) #transpose to output to a new data frame with the column names as row names. And make all data numeric
+
+#Check for genes and samples with too many missing values with goodSamplesGenes. There shouldn't be any because we performed pre-filtering
+gsg = goodSamplesGenes(datExpr, verbose = 3)
+gsg$allOK #Should return TRUE if not, the R chunk below will take care of flagged data
+# [1] allOK is TRUE
+
+#Remove flagged samples if the allOK is FALSE, not used here.  
+
+#ncol(datExpr) #number genes before
+#if (!gsg$allOK) #If the allOK is FALSE...
+#{
+# Optionally, print the gene and sample names that are flagged:
+#if (sum(!gsg$goodGenes)>0)
+#printFlush(paste("Removing genes:", paste(names(datExpr)[!gsg$goodGenes], collapse = ", ")));
+#if (sum(!gsg$goodSamples)>0)
+#printFlush(paste("Removing samples:", paste(rownames(datExpr)[!gsg$goodSamples], collapse = ", ")));
+# Remove the offending genes and samples from the data:
+#datExpr = datExpr[gsg$goodSamples, gsg$goodGenes]
+#}
+#ncol(datExpr) #number genes after
+
+#Cluster the samples to look for obvious outliers
+#Look for outliers by examining the sample tree:
+
+sampleTree = hclust(dist(datExpr), method = "average")
+
+#Plot the sample tree
+pdf(paste0('sampleTree','.pdf'))
+plot(sampleTree, main = "Sample clustering to detect outliers", sub="", xlab="", cex.lab = 1.5, cex.axis = 1.5, cex.main = 2)
+dev.off()
+
+
+#PCA--------------------------------------------------------------------------------------------------------------
+gPCAdata <- plotPCA(gvst, intgroup = c("timepoint", "Species"), returnData=TRUE, ntop=5000)
+
+percentVar <- round(100 * attr(gPCAdata, "percentVar"))
+
+
+
+# Ensure timepoint and species are factors with correct levels
+gPCAdata$timepoint <- factor(gPCAdata$timepoint, levels = c("I", "II", "III"))
+gPCAdata$Species <- factor(gPCAdata$Species, levels = c("Acropora_tenuis", "Montipora_capitata", "Pocillopora_acuta", "Stylophora_pistillata"))
+
+# Create PCA visualization
+allgenesfilt_PCA_visual <- 
+  ggplot(data = gPCAdata, aes(x = PC1, y = PC2, color = timepoint, shape = Species)) + 
+  geom_point(size = 5) +
+  xlab(paste0("PC1: ", percentVar[1], "% variance")) +
+  ylab(paste0("PC2: ", percentVar[2], "% variance")) +
+  ylim(-50, 50) +
+  coord_fixed() +
+  theme_classic() +
+  theme(
+    panel.border = element_blank(),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.line = element_line(colour = "black"),
+    plot.background = element_blank()
+  ) +
+  scale_shape_manual(values = c(15, 16, 17, 18)) # Different shapes for each species
+
+# Print the PCA plot
+print(allgenesfilt_PCA_visual)
+
+
+#Choose a set of soft-thresholding powers
+powers <- c(seq(from = 1, to=19, by=2), c(21:30)) #Create a string of numbers from 1 through 10, and even numbers from 10 through 20
+powerVector = c(seq(1, 10, by = 1), seq(12, 20, by = 1))
+
+# Load the doParallel package
+library(doParallel)
+
+# Register a parallel backend with the desired number of cores
+# Adjust the number of cores as needed
+registerDoParallel(cores = 4)  # Set the number of cores
+#Call the network topology analysis function
+sft <-pickSoftThreshold(datExpr, powerVector = powers, verbose = 5)
+
+# Don't forget to stop the parallel backend when you're done
+#stopImplicitCluster()
+
+# This will unregister the parallel backend and prevent further messages
+
+#Plot the results
+sizeGrWindow(9, 5)
+par(mfrow = c(1, 2))
+cex1 = 0.9
+
+#Scale-free topology fit index as a function of the soft-thresholding power
+pdf(paste0('network', '.pdf'))
+plot(sft$fitIndices[, 1], -sign(sft$fitIndices[, 3]) * sft$fitIndices[, 2],
+     xlab = "Soft Threshold (power)", ylab = "Scale Free Topology Model Fit, signed R^2", type = "n",
+     main = paste("Scale independence"))
+text(sft$fitIndices[, 1], -sign(sft$fitIndices[, 3]) * sft$fitIndices[, 2],
+     labels = powers, cex = cex1, col = "red")
+abline(h = 0.8, col = "red")
+
+# Mean connectivity as a function of the soft-thresholding power
+plot(sft$fitIndices[, 1], sft$fitIndices[, 5],
+     xlab = "Soft Threshold (power)", ylab = "Mean Connectivity", type = "n",
+     main = paste("Mean connectivity"))
+text(sft$fitIndices[, 1], sft$fitIndices[, 5], labels = powers, cex = cex1, col = "red")
+dev.off()
+
+
+#The lowest scale-free topology fit index R^2 recommended by Langfelder and Horvath is 0.8. 
+#From the graph, it appears that our soft thresholding power is 19 because it is the lowest 
+#power before the R^2=0.8 threshold that maximizes with model fit (s17 is the number right above the red line).
+
+### Network construction and module detection:
+# Co-expression adjacency and topological overlap matrix similarity
+# Co-expression similarity and adjacency, using the soft thresholding power 19 and translate the adjacency into topological overlap matrix to calculate 
+# the corresponding dissimilarity. I will use a signed network because we have a relatively high softPower, according 
+# to >12 (https://peterlangfelder.com/2018/11/25/__trashed/). 
+#Moreover, in expression data where you are interested in when expression on one gene increases or decreases with expression level of another you would use a signed network (when you are interested in the direction of change, correlation and anti-correlation, you use a signed network).
+
+options(stringsAsFactors = FALSE)
+enableWGCNAThreads() #Allow multi-threading within WGCNA
+
+
+#Version two of this graph (with dots)
+dev.new()
+
+
+a1 <- ggplot(sft$fitIndices, aes(Power, SFT.R.sq, label = Power)) +
+  geom_point() +
+  geom_text(nudge_y = 0.1) +
+  geom_hline(yintercept = 0.8, color = 'red') +
+  labs(x = 'Power', y = 'Scale free topology model fit, signed R^2') +
+  theme_classic()
+
+a2 <- ggplot(sft$fitIndices, aes(Power, mean.k., label = Power)) +
+  geom_point() +
+  geom_text(nudge_y = 0.1) +
+  labs(x = 'Power', y = 'Mean Connectivity') +
+  theme_classic()
+
+grid.arrange(a1, a2, nrow = 2)
+
+
 
